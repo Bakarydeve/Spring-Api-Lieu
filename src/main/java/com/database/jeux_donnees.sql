c21e067ebbe76f77a8c8b7f4a019ae806c948f99
baki <bbaki9919@gmail.com>
2024-03-06 11:26:16 +0100

    database readme specification

---
+++ src/main/java/com/database/jeux_donnees.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO `lieu` (`id`, `address`, `name`) VALUES (NULL, 'Rue de Kervao, 29820 Bohars', 'Le Moulin du Ponchou');
+INSERT INTO `lieu` (`id`, `address`, `name`) VALUES (NULL, 'Manoir de Lossulien, 29480 Le Relecq-Kerhuon', 'Domaine de Lossulien');
+INSERT INTO `lieu` (`id`, `address`, `name`) VALUES (NULL, '140 Bd de Plymouth, 29200 Brest', 'Arena');
